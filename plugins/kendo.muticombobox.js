2e2165231d7477c6b863e4208fae340c38132751
James Maxwell <james.maxwell.hu@gmail.com>
2012-12-25 17:16:33 +0800

    add setValue method

--- plugins/kendo.muticombobox.js
+++ plugins/kendo.muticombobox.js
@@ -4,8 +4,8 @@
 *
 * Support MutiSelect for ComboBox.
 */
-(function($){
-	var kendo = window.kendo,
+(function ($) {
+    var kendo = window.kendo,
 	ui = kendo.ui,
 	ComboBox = ui.ComboBox,
 	ns = ".kendoMutiComboBox",
@@ -14,66 +14,86 @@
     STATE_FILTER = "filter",
     STATE_ACCEPT = "accept",
 	ALL_HEIGHT = 25;
-	
-	var MutiComboBox = ComboBox.extend({
-		init: function(element, options){
-			var that = this, 
+
+    var MutiComboBox = ComboBox.extend({
+        init: function (element, options) {
+            var that = this,
 			h,
 			selAll,
 			allText = options.allText || that.options.allText;
-			
-			options.template = '<span style="white-space:nowrap"><label><input type="checkbox"/>${data' + (options.dataTextField ? "." : "") + options.dataTextField + '}</label></span>';
-			
-			ComboBox.fn.init.call(that, element, options);
-			
-			selAll = $('<div class="k-reset"/>')
+
+            options.template = '<span style="white-space:nowrap"><label><input type="checkbox" key="${data' + (options.dataValueField ? "." : "") + options.dataValueField + '}"/>${data' + (options.dataTextField ? "." : "") + options.dataTextField + '}</label></span>';
+            ComboBox.fn.init.call(that, element, options);
+
+            selAll = $('<div class="k-reset"/>')
 				.height(ALL_HEIGHT)
-				.css({'border-bottom':'1px solid #fff','margin-bottom':'2px','padding-left':'5px'})
+				.css({ 'border-bottom': '1px solid #fff', 'margin-bottom': '2px', 'padding-left': '5px' })
 				.append('<label><input type="checkbox" name="_cmbAll"/>' + allText + '</label>')
 				.prependTo(that.list);
-			
-			selAll.find('input').on('click', function(e){ 
-				that.list.find('input').prop('checked',$(this).prop('checked'));
-				that._select(that.list.find('li').get(0));
-			});
-			
-			kendo.notify(that);
-		},
-		
-		options: {
-			name: 'MutiComboBox',
-			enable: true,
-			allText: 'all'
-		},
-		
-		_click: function(e) {
+
+            selAll.find('input').on('click', function (e) {
+                that.list.find('input').prop('checked', $(this).prop('checked'));
+                that._select(that.list.find('li').get(0));
+            });
+
+            kendo.notify(that);
+        },
+
+        options: {
+            name: 'MutiComboBox',
+            enable: true,
+            allText: 'all'
+        },
+
+        setValue: function (values) {
+            var that = this, texts = [], sels = 0;
+
+            if (typeof (values) === 'string')
+                values = values.split(',');
+
+            that.list.find('input[name!=_cmbAll]').each(function (i) {
+                if ($.inArray($(this).attr('key'), values) != -1) {
+                    $(this).prop('checked', true);
+                    texts.push($(this).parent().text());
+                    sels++;
+                }
+            });
+
+            if (that.list.find('input[name!=_cmbAll]').size() == sels) {
+                that.list.find('input[name=_cmbAll]').prop('checked', true);
+            }
+
+            that.input[0].value = texts.join(',');
+        },
+
+        _click: function (e) {
             if (!e.isDefaultPrevented()) {
                 this._accept($(e.currentTarget));
-				
+
                 if (e.type === "touchend") {
                     e.preventDefault();
                 }
             }
-			e.stopPropagation();
+            e.stopPropagation();
         },
-	
-		_blur: function() {
+
+        _blur: function () {
             var that = this;
 
             that._change();
             //that.close(); //not close in MutiCombobox
         },
-		
-		_select: function(li) {
+
+        _select: function (li) {
             var that = this,
                 texts = [],
                 values = [],
                 datas = that._data(),
                 idx = that._highlight(li);
 
-			if($.isFunction(li))
-				return;
-				
+            if ($.isFunction(li))
+                return;
+
             that.selectedIndex = idx;
 
             if (idx !== -1) {
@@ -82,20 +102,20 @@
                 }
 
                 that._current.addClass(STATE_SELECTED);
-				
-				that.list.find('input[name!=_cmbAll]').each(function(i){
-					if($(this).prop('checked') === true){
-						data = datas[i];
-						texts.push(that._text(data));
-						values.push(that._value(data));
-					}
-				});
 
-				that.list.find('input[name=_cmbAll]').prop('checked',
+                that.list.find('input[name!=_cmbAll]').each(function (i) {
+                    if ($(this).prop('checked') === true) {
+                        data = datas[i];
+                        texts.push(that._text(data));
+                        values.push(that._value(data));
+                    }
+                });
+
+                that.list.find('input[name=_cmbAll]').prop('checked',
 					that.list.find('input[name!=_cmbAll]').size() == that.list.find('input[name!=_cmbAll]:checked').size());
 
                 that._prev = that._text(datas[idx]);
-				that.input[0].value = texts.join(',');
+                that.input[0].value = texts.join(',');
                 that._accessor(values.join(','), idx);
                 that._placeholder();
 
@@ -104,7 +124,7 @@
                 }
             }
         }
-	});
-	
-	ui.plugin(MutiComboBox);
+    });
+
+    ui.plugin(MutiComboBox);
 })(jQuery);
